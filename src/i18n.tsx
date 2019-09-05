a97cfab5fa4da46718930eb0f164e4ebaabc4a26
lujianan <>
2019-09-05 18:28:59 +0800

    加入i18n-react国际化处理库，一些初始化和api调试

---
+++ src/i18n.tsx
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import * as en from './i18json/en.json'
+import * as zn from './i18json/zn.json'
+import * as dict from './i18json/dict.json'
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources:{
+      en,
+      zn,
+      dict
+    },
+    lng: "zn",
+    // ns: ['moduleA','moduleB'],
+    defaultNS: 'moduleA', // 如果不写，只会加载namespace为默认的translation的模块
+    keySeparator: false, // we do not use keys in form messages.welcome
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    }
+  });
+
+  export default i18n;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,100 +1,180 @@
-import * as React from 'react'
-import {withRouter} from "react-router-dom"
-import { Table, Skeleton } from 'antd'
-import Ellipsis from '../components/Ellipsis'
-import Button from '../components/Button'
-import ButtonGroups from '../components/ButtonGroups'
+import * as React from "react";
+import { withRouter } from "react-router-dom";
+import { Table, Skeleton, Select } from "antd";
+import { compose } from "recompose";
+import Ellipsis from "../components/Ellipsis";
+import Button from "../components/Button";
+import ButtonGroups from "../components/ButtonGroups";
+import i18n from "../i18n";
+import { Trans } from "react-i18next";
+import { withTranslation } from "react-i18next";
+import { Translation } from "react-i18next";
+
+const Option = Select.Option;
 
 export interface Props {
-    history:any;
+  history: any;
 }
-  
+
 interface State {
-    show?: boolean;
+  show?: boolean;
 }
 
 const dataSource = [
-    {
-      key: '1',
-      name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号',
-    },
-    {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号',
-    },
-  ];
-  
-  const columns = [
-    {
-      title: '姓名',
-      dataIndex: 'name',
-      key: 'name',
-      render: (text:string) => Ellipsis( {text} )
-    },
-    {
-      title: '年龄',
-      dataIndex: 'age',
-      key: 'age',
-      render: (text:string) => Ellipsis( {text} )
-    },
-    {
-      title: '住址',
-      dataIndex: 'address',
-      key: 'address',
-      render: (text:string) => Ellipsis( {text} )
-    },
-  ];
+  {
+    key: "1",
+    name: "胡彦斌",
+    age: 32,
+    address: "西湖区湖底公园1号"
+  },
+  {
+    key: "2",
+    name: "胡彦祖",
+    age: 42,
+    address: "西湖区湖底公园1号"
+  }
+];
 
-class Index extends React.Component<Props, State>{
-    constructor(props:any){
-      super(props)
-    }
-    handleClick = (type:string,key:string) => {
-      // console.log(type)
-      console.log(key)
-    }
-    render(){
-        return (
-            <React.Fragment>
-                <ButtonGroups handleClick={this.handleClick.bind(this, 'type')} viewMode="icon" showSize={2} mode="ButtonMenu">
-                    <Button permission={true} actionkey="aa">aa</Button>
-                    <Button permission={false} actionkey="cc" confirm="cccccccccccccccc" tip="ccccccccccccccccc">cc</Button>
-                    <Button permission={true} actionkey="bb" icon="apple">bb</Button>
-                    <Button permission={true} actionkey="dd">dd</Button>
-                    <Button permission={true} actionkey="ee">ee</Button>
-                    <Button permission={true} actionkey="ff" confirm="ffffffffffffffff" tip="fffffffffffffff">ff</Button>
-                    <Button permission={true} actionkey="gg" disabled={true}>gg</Button>
-                </ButtonGroups>
-                <Table dataSource={dataSource} columns={columns} />
-                <Button onClick={()=> this.props.history.push('/first')}>添加</Button>
-                <Skeleton 
-                  active={true} 
-                  paragraph={{
-                    rows: 5,
-                    width: ['50%','60%','70%','80%','90%']
-                  }}
-                  title={true}
-                  avatar={{
-                    size: 'large',
-                    shape: 'square'
-                  }} 
-                >
-                  <div>
-                    <h4>Ant Design, a design language</h4>
-                    <p>
-                      We supply a series of design principles, practical patterns and high quality design
-                      resources (Sketch and Axure), to help people create their product prototypes
-                      beautifully and efficiently.
-                    </p>
-                  </div>
-                </Skeleton>
-            </React.Fragment>
-        )
-    }
+class Index extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+  }
+  componentDidMount() {
+    console.log(i18n.t("tree", { returnObjects: true, something: "gold" }));
+    // console.log(i18n.t("array", { returnObjects: true }));
+  }
+  handleClick = (type: string, key: string) => {
+    // console.log(type)
+    console.log(key);
+  };
+  handleChange = (type: string) => {
+    i18n.changeLanguage(type);
+    // console.log(i18n.t("dbType", { returnObjects: true }));
+    console.log(i18n.t("moduleC:namespace"));
+  };
+  render() {
+    const columns = [
+      {
+        title: i18n.t("table.name"),
+        dataIndex: "name",
+        key: "name",
+        render: (text: string) => Ellipsis({ text })
+      },
+      {
+        title: i18n.t("table.age"),
+        dataIndex: "age",
+        key: "age",
+        render: (text: string) => Ellipsis({ text })
+      },
+      {
+        title: i18n.t("table.address"),
+        dataIndex: "address",
+        key: "address",
+        render: (text: string) => Ellipsis({ text })
+      }
+    ];
+    const person = {
+      name: "i18n",
+      age: "26",
+      sex: "male"
+    };
+    return (
+      <React.Fragment>
+        <Select
+          defaultValue="zn"
+          onChange={this.handleChange}
+          style={{ width: 150, float: "right" }}
+        >
+          <Option value="en">English</Option>
+          <Option value="zn">中文</Option>
+          <Option value="dict">字典</Option>
+        </Select>
+        <ButtonGroups
+          handleClick={this.handleClick.bind(this, "type")}
+          viewMode="icon"
+          showSize={2}
+          mode="ButtonMenu"
+        >
+          <Button permission={true} actionkey="aa">
+            aa
+          </Button>
+          <Button
+            permission={false}
+            actionkey="cc"
+            confirm="cccccccccccccccc"
+            tip="ccccccccccccccccc"
+          >
+            cc
+          </Button>
+          <Button permission={true} actionkey="bb" icon="apple">
+            bb
+          </Button>
+          <Button permission={true} actionkey="dd">
+            dd
+          </Button>
+          <Button permission={true} actionkey="ee">
+            ee
+          </Button>
+          <Button
+            permission={true}
+            actionkey="ff"
+            confirm="ffffffffffffffff"
+            tip="fffffffffffffff"
+          >
+            ff
+          </Button>
+          <Button permission={true} actionkey="gg" disabled={true}>
+            gg
+          </Button>
+        </ButtonGroups>
+        <Table dataSource={dataSource} columns={columns} />
+        <Button onClick={() => this.props.history.push("/first")}>
+          {i18n.t("add")}
+        </Button>
+        <Skeleton
+          active={true}
+          paragraph={{
+            rows: 5,
+            width: ["50%", "60%", "70%", "80%", "90%"]
+          }}
+          title={true}
+          avatar={{
+            size: "large",
+            shape: "square"
+          }}
+        >
+          <div>
+            <h4>Ant Design, a design language</h4>
+            <p>
+              We supply a series of design principles, practical patterns and
+              high quality design resources (Sketch and Axure), to help people
+              create their product prototypes beautifully and efficiently.
+            </p>
+          </div>
+        </Skeleton>
+        <h1>{i18n.t("Welcome to React")}</h1>
+        <h2>{i18n.t("name", { person })}</h2>
+        <h1>{i18n.t("aaa")}</h1>
+        <h1>{i18n.t("nesting1", { number: "$t(nesting2)" })}</h1>
+        <h1>{i18n.t("dbType", { context: "1" })}</h1>
+
+        {/* 代码段内使用指定namespace */}
+        <Translation ns="moduleC">{t => <p>{t("namespace")}</p>}</Translation>
+        {/* 指定多个namespace，则把第一个作为默认，找不到就找不到，不去匹配后面的namespace */}
+        <Translation ns={['moduleA', 'moduleB', 'moduleC']}>{t => <p>{t("namespace")}</p>}</Translation>
+        <p>{i18n.t("namespace")}</p>
+
+        <Trans
+          i18nKey="welcomeUser"
+          values={{ name: "tomorrow", customer: "yesterday" }}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
-export default withRouter(Index)
+export default compose(
+  withTranslation(),
+  withRouter
+)(Index);
+// export default withRouter(Index)
